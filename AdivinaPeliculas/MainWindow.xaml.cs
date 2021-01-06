7d838f93fd82b776c3ea77aab7a15b6261031e86
Alejandro Mujica <alejandro.mujica@iesdoctorbalmis.com>
2021-01-06 20:41:30 +0100

    Agregar archivos de proyecto.

---
+++ AdivinaPeliculas/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+
+using Newtonsoft.Json;
+using Microsoft.Win32;
+
+namespace AdivinaPeliculas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Pelicula> listaPeliculas;
+        List<string> listaGenerosComboBox;
+        String archivoJson;
+        public MainWindow()
+        {
+            InitializeComponent();
+            listaGenerosComboBox = new List<string> { "Comedia", "Drama", "Accion", "Terror", "Sci-fi" };
+
+            listaPeliculas = new ObservableCollection<Pelicula>();
+
+            PeliculasListBox.DataContext = listaPeliculas;
+            generoPeliculaComboBox.ItemsSource = listaGenerosComboBox;
+        }
+
+        private void guardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            string peliculasJson = JsonConvert.SerializeObject(listaPeliculas);
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Json files (*.json;)|*.json;|All files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, peliculasJson);
+        }
+
+        private void cargarButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Json files (*.json;)|*.json;|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                archivoJson = openFileDialog.FileName;
+
+            listaPeliculas.Clear();
+            using (StreamReader jsonStream = File.OpenText(archivoJson))
+            {
+                var json = jsonStream.ReadToEnd();
+                List<Pelicula> peliculas = JsonConvert.DeserializeObject<List<Pelicula>>(json);
+
+                foreach (Pelicula p in peliculas)
+                {
+                    listaPeliculas.Add(p);
+                }
+            }
+        }
+
+        private void eliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            listaPeliculas.Remove((Pelicula)PeliculasListBox.SelectedItem);
+        }
+
+        private void añadirButton_Click(object sender, RoutedEventArgs e)
+        {
+            listaPeliculas.Add(new Pelicula());
+        }
+
+        private void examinarButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpg)|*.png;*.jpg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                imagenTextBox.Text = openFileDialog.FileName;
+        }
+    }
+}
---
+++ AdivinaPeliculas/Pelicula.cs
@@ -0,0 +1,127 @@
+﻿using System.ComponentModel;
+
+namespace AdivinaPeliculas
+{
+    class Pelicula : INotifyPropertyChanged
+    {
+        private string nombre;
+        private string pista;
+        private string imagen;
+        private bool facil;
+        private bool normal;
+        private bool dificil;
+        private string genero;
+
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (nombre != value)
+                {
+                    nombre = value;
+                    NotifyPropetyChanged("Nombre");
+                }
+            }
+        }
+        public string Pista
+        {
+            get => pista;
+            set
+            {
+                if (pista != value)
+                {
+                    pista = value;
+                    NotifyPropetyChanged("Pista");
+                }
+            }
+        }
+        public string Imagen
+        {
+            get => imagen;
+            set
+            {
+                if (imagen != value)
+                {
+                    imagen = value;
+                    NotifyPropetyChanged("Imagen");
+                }
+            }
+        }
+        public bool Facil
+        {
+            get => facil;
+            set
+            {
+                if (facil != value)
+                {
+                    facil = value;
+                    NotifyPropetyChanged("Facil");
+                }
+            }
+        }
+        public bool Normal
+        {
+            get => normal;
+            set
+            {
+                if (normal != value)
+                {
+                    normal = value;
+                    NotifyPropetyChanged("Normal");
+                }
+            }
+        }
+        public bool Dificil
+        {
+            get => dificil;
+            set
+            {
+                if (dificil != value)
+                {
+                    dificil = value;
+                    NotifyPropetyChanged("Dificil");
+                }
+            }
+        }
+        public string Genero
+        {
+            get => genero;
+            set
+            {
+                if (genero != value)
+                {
+                    genero = value;
+                    NotifyPropetyChanged("Genero");
+                }
+            }
+        }
+
+        public Pelicula()
+        {
+            Nombre = "Nueva Pelicula";
+            Facil = true;
+            Genero = "Comedia";
+        }
+
+        public Pelicula(string nombre, string pista, string imagen, 
+            bool facil, bool normal, bool dificil, string genero)
+        {
+            Nombre = nombre;
+            Pista = pista;
+            Imagen = imagen;
+            Facil = facil;
+            Normal = normal;
+            Dificil = dificil;
+            Genero = genero;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropetyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
